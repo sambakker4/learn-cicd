26922f761720daea9b05b27a8e0d626a6e52d7cf
sambakker4 <sambakkercow@gmail.com>
2025-04-27 09:21:00 -0400

    fixed code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,11 +12,9 @@ var MalformedHeader = errors.New("malformed authorization header")
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
-	/*
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
 	}
-	*/
 	
 	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
